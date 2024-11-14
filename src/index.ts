3811c6be1fe9d39a42c535d759fb61dcae163bb7
Matheus Ribas <matheusribas2013@gmail.com>
2024-11-13 23:31:14 -0300

    Adicionado endpoint para buscar todos os usuarios

--- src/index.ts
+++ src/index.ts
@@ -383,6 +383,30 @@ app.put("/recipes/:id", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+// ENDPOINTS DO USERS
+
+// BUSCAR TODOS OS USUÁRIOS
+app.get("/users", async (req: Request, res: Response): Promise<void> => {
+  const page = Number(req.query.page) || 1; // Pagina atual
+  const limit = Number(req.query.limit) || 10; // Número de itens por pagina
+  const offset = (page - 1) * limit; // Calculo do offset
+
+  try {
+    const users = await connection("users")
+      .select("*") // Seleciona todos os campos da tabela users
+      .limit(limit)
+      .offset(offset);
+
+    if (users.length === 0) {
+      throw new Error("No users found");
+    }
+
+    res.status(200).json(users);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message || "Error fetching users" });
+  }
+});
+
 
 
 // Iniciar o servidor
